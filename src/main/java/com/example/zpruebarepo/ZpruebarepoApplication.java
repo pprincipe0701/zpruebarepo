7fef60ba3c545350fb83d69bdec71c95234d0dd1
Pavel Principex <pprincipe.cons@outlook.com>
2022-10-25 11:29:02 -0500

    segunda subida

--- src/main/java/com/example/zpruebarepo/ZpruebarepoApplication.java
+++ src/main/java/com/example/zpruebarepo/ZpruebarepoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ZpruebarepoApplication {
-//pprincipe
+//pprincipe1212
 	public static void main(String[] args) {
 		SpringApplication.run(ZpruebarepoApplication.class, args);
 	}
