b395078a1f89ab12852e556f79070781a3a2c2ea
Pavel Principex <pprincipe.cons@outlook.com>
2022-10-25 11:27:20 -0500

    Primera subida

---
+++ src/main/java/com/example/zpruebarepo/ZpruebarepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.zpruebarepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZpruebarepoApplication {
+//pprincipe
+	public static void main(String[] args) {
+		SpringApplication.run(ZpruebarepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zpruebarepo/ZpruebarepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zpruebarepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZpruebarepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
